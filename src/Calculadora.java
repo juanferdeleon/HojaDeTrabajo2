13dcfc093ca5532fd8e10e4d5dc8b55d1cb5ebfe
Juan De Leon <dele17822@uvg.edu.gt>
2019-02-01 18:13:31 -0600

    Pruebas

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -7,44 +7,48 @@ public class Calculadora implements iCalculadora {
 
     /**
      * Método para hacer la suma de la calculadora
+     *
      * @param a double - sumando
      * @param b double - sumando
      * @return - El resultado de la suma de a y b
      */
-    public double sumar(double a, double b){
+    public double sumar(double a, double b) {
         this.resultado = a + b;
         return this.resultado;
     }
 
     /**
      * Método para hacer la resta de la calculadora
+     *
      * @param a double -
      * @param b double -
      * @return double - El resultado de la resta entre a y b
      */
-    public double restar(double a, double b){
+    public double restar(double a, double b) {
         this.resultado = a - b;
         return this.resultado;
     }
 
     /**
      * Método para hacer la multiplicación de la calculadora
+     *
      * @param a double - factor
      * @param b double - factor
      * @return double - El resultado de la multiplicación entre a y b
      */
-    public double multiplicar(double a, double b){
+    public double multiplicar(double a, double b) {
         this.resultado = a * b;
         return this.resultado;
     }
 
     /**
      * Método para hacer la división de la calculadora
+     *
      * @param a double - dividendo
      * @param b double - divisor
      * @return double - El resultado de la división entre a y b
      */
-    public double dividir(double a, double b){
+    public double dividir(double a, double b) {
         this.resultado = a / b;
         return this.resultado;
     }
---
+++ src/CalculadoraTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraTest {
+
+    @org.junit.jupiter.api.Test
+    void sumar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void restar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiplicar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void dividir() {
+    }
+}
